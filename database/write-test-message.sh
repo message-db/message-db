858844f552df06910219d45505dda9eb8fe9a5d5
Scott Bellware <sbellware@gmail.com>
2018-06-11 18:12:12 -0500

    Test message script allows conrol of database name and user

--- database/write-test-message.sh
+++ database/write-test-message.sh
@@ -17,10 +17,31 @@ echo "Writing $instances Messages to Stream $stream_name"
 echo "= = ="
 echo
 
+default_name=message_store
+
+if [ -z ${DATABASE_USER+x} ]; then
+  echo "(DATABASE_USER is not set)"
+  user=$default_name
+else
+  user=$DATABASE_USER
+fi
+echo "Database user is: $user"
+
+if [ -z ${DATABASE_NAME+x} ]; then
+  echo "(DATABASE_NAME is not set)"
+  database=$default_name
+else
+  database=$DATABASE_NAME
+fi
+echo "Database name is: $database"
+echo
+
+
 for (( i=1; i<=instances; i++ )); do
   uuid=$(uuidgen)
   echo "Instance: $i, Message ID: $uuid"
-  psql message_store -c "SELECT write_message('$uuid'::varchar, '$stream_name'::varchar, 'SomeType'::varchar, '{\"attribute\": \"some value\"}'::jsonb, '{\"metaAttribute\": \"some meta value\"}'::jsonb);" > /dev/null
+
+  psql $database -c "SELECT write_message('$uuid'::varchar, '$stream_name'::varchar, 'SomeType'::varchar, '{\"attribute\": \"some value\"}'::jsonb, '{\"metaAttribute\": \"some meta value\"}'::jsonb);" > /dev/null
 done
 
 echo
