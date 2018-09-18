71a0814be75f912a1cc64f454a21f2ad0e292970
Scott Bellware <sbellware@gmail.com>
2018-09-17 20:31:17 -0700

    Views are granted privileges on the message_store role

--- database/user/privileges.sql
+++ database/user/privileges.sql
@@ -1,7 +1,10 @@
+-- Table
 GRANT SELECT, INSERT ON messages TO message_store;
 
+-- Sequence
 GRANT USAGE, SELECT ON SEQUENCE messages_global_position_seq TO message_store;
 
+-- Functions
 GRANT EXECUTE ON FUNCTION gen_random_uuid TO message_store;
 GRANT EXECUTE ON FUNCTION md5(text) TO message_store;
 GRANT EXECUTE ON FUNCTION hash_64 TO message_store;
@@ -11,3 +14,11 @@ GRANT EXECUTE ON FUNCTION write_message TO message_store;
 GRANT EXECUTE ON FUNCTION get_stream_messages TO message_store;
 GRANT EXECUTE ON FUNCTION get_category_messages TO message_store;
 GRANT EXECUTE ON FUNCTION get_last_message TO message_store;
+
+-- Views
+GRANT SELECT, INSERT ON category_type_summary TO message_store;
+GRANT SELECT, INSERT ON stream_summary TO message_store;
+GRANT SELECT, INSERT ON stream_type_summary TO message_store;
+GRANT SELECT, INSERT ON type_category_summary TO message_store;
+GRANT SELECT, INSERT ON type_stream_summary TO message_store;
+GRANT SELECT, INSERT ON type_summary TO message_store;
