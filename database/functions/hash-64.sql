d745ecfce1e834e3e04ba44e634043e92e211b93
Nathan Ladd <nathanladd+github@gmail.com>
2018-09-27 16:50:15 -0500

    hash_64 function produces values within the full range of 64-bit numbers

--- database/functions/hash-64.sql
+++ database/functions/hash-64.sql
@@ -6,7 +6,7 @@ AS $$
 DECLARE
   hash bigint;
 BEGIN
-  select left('x' || md5(_stream_name), 16)::bit(64)::bigint into hash;
+  select left('x' || md5(_stream_name), 17)::bit(64)::bigint into hash;
   return hash;
 END;
 $$ LANGUAGE plpgsql
