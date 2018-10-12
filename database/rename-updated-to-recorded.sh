88490fa95281b5e439f6dad66b4dc1f64da3cd64
Nathan Ladd <nathanladd+github@gmail.com>
2018-10-12 13:31:49 -0500

    Obsolete use of DATABASE_USER env variable is removed

--- database/rename-updated-to-recorded.sh
+++ database/rename-updated-to-recorded.sh
@@ -6,14 +6,6 @@ echo
 
 default_name=message_store
 
-if [ -z ${DATABASE_USER+x} ]; then
-  echo "(DATABASE_USER is not set)"
-  user=$default_name
-else
-  user=$DATABASE_USER
-fi
-echo "Database user is: $user"
-
 if [ -z ${DATABASE_NAME+x} ]; then
   echo "(DATABASE_NAME is not set)"
   database=$default_name
