585f702a179e52c555ea0ab1a4f80d682b28f060
Scott Bellware <sbellware@gmail.com>
2018-10-28 16:25:51 -0500

    Database user cannot be set for clearing messages

--- database/clear-messages.sh
+++ database/clear-messages.sh
@@ -9,14 +9,6 @@ echo
 
 default_name=message_store
 
-if [ -z ${DATABASE_USER+x} ]; then
-  echo "(DATABASE_USER is not set)"
-  user=$default_name
-else
-  user=$DATABASE_USER
-fi
-echo "Database user is: $user"
-
 if [ -z ${DATABASE_NAME+x} ]; then
   echo "(DATABASE_NAME is not set)"
   database=$default_name
@@ -27,4 +19,4 @@ echo "Database name is: $database"
 
 echo
 
-psql $database -U $user -c "TRUNCATE messages RESTART IDENTITY;"
+psql $database -c "TRUNCATE messages RESTART IDENTITY;"
