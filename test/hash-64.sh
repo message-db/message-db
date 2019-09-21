ab9e4b017e1d6539c18479fed266860b8c65f92f
Scott Bellware <sbellware@gmail.com>
2019-09-21 17:35:43 -0500

    Hash 64 test uses random stream name

--- test/hash-64.sh
+++ test/hash-64.sh
@@ -5,7 +5,15 @@ echo "GET HASH 64"
 echo "==========="
 echo
 
-psql message_store -U message_store -x -c "SELECT hash_64('someStream-123');"
+source test/controls.sh
+
+stream_name=$(stream-name)
+
+echo "Stream Name:"
+echo $stream_name
+echo
+
+psql message_store -U message_store -x -c "SELECT hash_64('$stream_name');"
 
 echo "= = ="
 echo
