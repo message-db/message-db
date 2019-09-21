f07cdfe8ff524301d08e5816c8bb394de702e1e3
Scott Bellware <sbellware@gmail.com>
2019-09-21 17:31:12 -0500

    Output is formatted vertically

--- test/hash-64.sh
+++ test/hash-64.sh
@@ -5,7 +5,7 @@ echo "GET HASH 64"
 echo "==========="
 echo
 
-psql message_store -U message_store -c "SELECT hash_64('someStream-123');"
+psql message_store -U message_store -x -c "SELECT hash_64('someStream-123');"
 
 echo "= = ="
 echo
