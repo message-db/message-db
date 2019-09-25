4b6886818ca37a2eb8b017ebf9035db654aad6a4
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2019-09-24 22:42:08 -0700

    Hash 64 function parameter names are being disambiguated

--- database/functions/hash-64.sql
+++ database/functions/hash-64.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE FUNCTION hash_64(
-  _stream_name varchar
+  stream_name varchar
 )
 RETURNS bigint
 AS $$
 DECLARE
   hash bigint;
 BEGIN
-  select left('x' || md5(_stream_name), 17)::bit(64)::bigint into hash;
+  select left('x' || md5(hash_64.stream_name), 17)::bit(64)::bigint into hash;
   return hash;
 END;
 $$ LANGUAGE plpgsql
