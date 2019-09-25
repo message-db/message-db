0e5d9c35a14e8244fdc03f93a580e335c0c9b25e
Scott Bellware <sbellware@gmail.com>
2019-09-24 19:23:05 -0500

    Obsolete test setup scripts are removed

--- test/setup-messages.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-STREAM_NAME=someStream-123 INSTANCES=4 database/write-test-message.sh
-STREAM_NAME=someStream-456 INSTANCES=4 database/write-test-message.sh
-STREAM_NAME=someOtherStream-456 INSTANCES=4 database/write-test-message.sh
--- test/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-test/recreate-database.sh
-test/setup-messages.sh
