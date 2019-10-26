a690b2cf9f084a5927c7a18828ffa82f9d0ad885
Scott Bellware <sbellware@gmail.com>
2019-10-25 19:13:46 -0500

    Get category messages with correlation

--- test/get-category-messages-correlated.sh
+++ test/get-category-messages-correlated.sh
@@ -6,49 +6,37 @@ echo
 echo "GET CATEGORY MESSAGES CORRELATED"
 echo "====================="
 echo "- Write 2 messages each to 3 entity streams in the same category"
-echo "- Retrieve a batch of 2 messages from the category, starting at global position 0 where the position is greater than or equal to 1 and the correlation stream name is someCorrelation"
+echo "- Retrieve a batch of 2 messages from the category, starting at global position 0 and matching the correlation stream name"
 echo
 
 source test/controls.sh
 
 category=$(category)
-
 echo "Category:"
 echo $category
 echo
 
-# correlation=$(stream-name)
+correlation=$(stream-name)
+echo "Correlation:"
+echo $correlation
+echo
 
-# echo "Correlation:"
-# echo $correlation
-# echo
-
-for i in {1..2}; do
+for i in {1..3}; do
   stream_name=$(stream-name $category)
-  correlation=$(stream-name)
 
   echo "Stream Name: $stream_name"
-  echo "Correlation: $correlation"
 
   write-message-correlated $stream_name 1
   write-message-correlated $stream_name 1 $correlation
 done
 
+metadata_condition="'(metadata->>''correlationStreamName'' like ''$correlation%'')'"
 
-stream_name=$(stream-name $category)
-correlation=$(stream-name)
-
-echo "Stream Name: $stream_name"
-echo "Correlation: $correlation"
+echo "Metadata Condition:"
+echo $metadata_condition
 echo
 
-write-message-correlated $stream_name 1
-write-message-correlated $stream_name 1 $correlation
-
-metadata_condition="'(metadata->>''correlationStreamName'' like ''$correlation%'')'"
-echo "Metadata Condition: $metadata_condition"
-
-cmd="SELECT * FROM get_category_messages('$category', 0, 1000, $metadata_condition);"
+cmd="SELECT * FROM get_category_messages('$category', 0, 2, condition => $metadata_condition);"
 
 echo "Command:"
 echo "$cmd"
