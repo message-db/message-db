45e609cea774c97ff4abe305e2c73fa47f5820db
Scott Bellware <sbellware@gmail.com>
2019-10-27 01:49:50 -0500

    Function names are emphasized in debug output

--- database/functions/get-category-messages.sql
+++ database/functions/get-category-messages.sql
@@ -48,7 +48,7 @@ BEGIN
       $3';
 
   if current_setting('message_store.debug_get', true) = 'on' OR current_setting('message_store.debug', true) = 'on' then
-    RAISE NOTICE 'get_category_messages';
+    RAISE NOTICE '* get_category_messages';
     RAISE NOTICE 'category_name ($1): %', get_category_messages.category_name;
     RAISE NOTICE 'position ($2): %', get_category_messages.position;
     RAISE NOTICE 'batch_size ($3): %', get_category_messages.batch_size;
