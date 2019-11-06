6b9205cfc60a674cef07964571ef4366d0d000b0
Scott Bellware <sbellware@gmail.com>
2019-11-05 21:26:39 -0600

    Index scripts are clarified using whitespace

--- database/indexes/messages-category-global-position.sql
+++ database/indexes/messages-category-global-position.sql
@@ -1,2 +1,5 @@
 DROP INDEX CONCURRENTLY IF EXISTS "messages_category_global_position_idx";
-CREATE INDEX CONCURRENTLY "messages_category_global_position_idx" ON "public"."messages" USING btree(category(stream_name) COLLATE "default" "pg_catalog"."text_ops" ASC NULLS LAST, "global_position" "pg_catalog"."int8_ops" ASC NULLS LAST);
+
+CREATE INDEX CONCURRENTLY "messages_category_global_position_idx" ON "public"."messages"
+  USING btree(category(stream_name) COLLATE "default" "pg_catalog"."text_ops" ASC NULLS LAST,
+    "global_position" "pg_catalog"."int8_ops" ASC NULLS LAST);
--- database/indexes/messages-id-uniq.sql
+++ database/indexes/messages-id-uniq.sql
@@ -1,2 +1,4 @@
 DROP INDEX CONCURRENTLY IF EXISTS "messages_id_uniq_idx";
-CREATE UNIQUE INDEX CONCURRENTLY "messages_id_uniq_idx" ON "public"."messages" USING btree(id ASC NULLS LAST);
+
+CREATE UNIQUE INDEX CONCURRENTLY "messages_id_uniq_idx" ON "public"."messages"
+  USING btree(id ASC NULLS LAST);
