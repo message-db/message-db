dbac105bc0694e802b555c22d41d599ee0dbab43
Scott Bellware <sbellware@gmail.com>
2019-11-05 21:26:29 -0600

    Metadata correlation index is a partial index on the correlation stream name where it's not null

--- database/indexes/messages-metadata-correlation.sql
+++ database/indexes/messages-metadata-correlation.sql
@@ -1,2 +1,5 @@
 DROP INDEX CONCURRENTLY IF EXISTS "messages_metadata_correlation_idx";
-CREATE INDEX CONCURRENTLY "messages_metadata_correlation_idx" ON "public"."messages" USING btree((metadata->>'correlationStreamName') ASC NULLS LAST);
+
+CREATE INDEX CONCURRENTLY "messages_metadata_correlation_idx" ON "public"."messages"
+  USING btree(category(metadata->>'correlationStreamName') ASC)
+  WHERE (category(metadata->>'correlationStreamName') is not null);
