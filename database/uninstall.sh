ce03fe4941cdf35d924e4b8f075427c0035f9f98
Scott Bellware <sbellware@gmail.com>
2019-11-06 22:48:44 -0600

    Vestigial code is removed from the uninstall script

--- database/uninstall.sh
+++ database/uninstall.sh
@@ -7,55 +7,19 @@ echo "Uninstalling Database"
 echo "= = ="
 echo
 
-default_name=message_store
-
-if [ -z ${DATABASE_USER+x} ]; then
-  echo "(DATABASE_USER is not set. Default will be used.)"
-  user=$default_name
-else
-  user=$DATABASE_USER
-fi
-echo "Database user is: $user"
-
 if [ -z ${DATABASE_NAME+x} ]; then
   echo "(DATABASE_NAME is not set. Default will be used.)"
-  database=$default_name
+  database=message_store
 else
   database=$DATABASE_NAME
 fi
 echo "Database name is: $database"
 echo
 
-# function delete-user {
-#   user_exists=`psql postgres -qtAXc "SELECT 1 FROM pg_roles WHERE rolname='$user'"`
-
-#   if [ "$user_exists" = "1" ]; then
-#     echo "Deleting database user \"$user\"..."
-#     dropuser $user
-#   else
-#     echo "Database user \"$user\" does not exist. Not deleting."
-#   fi
-
-#   echo
-# }
-
 function delete-user {
-  psql -P pager=off -c "DROP ROLE IF EXISTS $user;"
+  psql -P pager=off -c "DROP ROLE IF EXISTS message_store;"
   echo
 }
-
-# function delete-database {
-#   database_exists=`psql postgres -qtAXc "SELECT 1 FROM pg_database WHERE datname='$database'"`
-
-#   if [ "$database_exists" = "1" ]; then
-#     echo "Deleting database \"$database\"..."
-#     dropdb $database
-#   else
-#     echo "Database \"$database\" does not exist. Not deleting."
-#   fi
-
-#   echo
-# }
 
 function delete-database {
   psql -P pager=off -c "DROP DATABASE IF EXISTS $database;"
