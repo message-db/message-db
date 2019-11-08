e9485e8ceb5525de8d18b8d37512c8c962ae7312
Scott Bellware <sbellware@gmail.com>
2019-11-08 00:12:55 -0600

    Vestigial files are removed

--- database/indexes/messages-stream-name-position-uniq.sql
+++
@@ -1,6 +0,0 @@
-DROP INDEX CONCURRENTLY IF EXISTS "messages_stream_name_position_uniq_idx";
-
-CREATE UNIQUE INDEX CONCURRENTLY "messages_stream_name_position_uniq_idx" ON "public"."messages"
-  USING btree(stream_name COLLATE "default" "pg_catalog"."text_ops" ASC NULLS LAST,
-    "position" "pg_catalog"."int8_ops" ASC NULLS LAST,
-    category(metadata->>'correlationStreamName') ASC);
