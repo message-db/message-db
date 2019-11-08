887844ee634420535505b6370d4c1cfd40fedcf1
Scott Bellware <sbellware@gmail.com>
2019-11-08 12:49:01 -0600

    Index definitions do not explicitly state options that are already defaults

--- database/indexes/messages-stream-name-position-correlation-uniq.sql
+++ database/indexes/messages-stream-name-position-correlation-uniq.sql
@@ -1,6 +1,7 @@
-DROP INDEX CONCURRENTLY IF EXISTS "messages_stream_name_position_correlation_uniq_idx";
+DROP INDEX CONCURRENTLY IF EXISTS messages_stream_name_position_correlation_uniq_idx;
 
-CREATE UNIQUE INDEX "messages_stream_name_position_correlation_uniq_idx" ON "public"."messages"
-  USING btree(stream_name COLLATE "default" "pg_catalog"."text_ops" ASC NULLS LAST,
-    "position" "pg_catalog"."int8_ops" ASC NULLS LAST,
-    category(metadata->>'correlationStreamName') ASC);
+CREATE UNIQUE INDEX messages_stream_name_position_correlation_uniq_idx ON messages (
+  stream_name,
+  position,
+  category(metadata->>'correlationStreamName')
+);
