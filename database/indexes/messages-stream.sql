9bfa91be2040a5cdfd21dd28291f6eaab60d81be
Scott Bellware <sbellware@gmail.com>
2019-12-01 22:58:41 -0600

    Correlation clause is removed from the stream index

--- database/indexes/messages-stream.sql
+++ database/indexes/messages-stream.sql
@@ -2,6 +2,5 @@ DROP INDEX IF EXISTS messages_stream;
 
 CREATE UNIQUE INDEX messages_stream ON messages (
   stream_name,
-  position,
-  category(metadata->>'correlationStreamName')
+  position
 );
