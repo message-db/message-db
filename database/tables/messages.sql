2583975919c6a504e5a57e4911021bae41649ba4
Aaron Jensen <aaronjensen@gmail.com>
2021-03-05 08:02:04 -0600

    Column ordering is further optimized
    In theory, jsonb columns have 4-byte alignment, and should therefore come before
    the text columns with 1-byte alignment.

--- database/tables/messages.sql
+++ database/tables/messages.sql
@@ -3,10 +3,10 @@ CREATE TABLE IF NOT EXISTS message_store.messages (
   position bigint NOT NULL,
   id UUID NOT NULL DEFAULT gen_random_uuid(),
   time TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'utc') NOT NULL,
-  stream_name text NOT NULL,
-  type text NOT NULL,
   data jsonb,
-  metadata jsonb
+  metadata jsonb,
+  stream_name text NOT NULL,
+  type text NOT NULL
 );
 
 ALTER TABLE message_store.messages ADD PRIMARY KEY (global_position) NOT DEFERRABLE INITIALLY IMMEDIATE;
