38a66c256ab87f444cd74eb8083baf50d8416d21
Scott Bellware <sbellware@gmail.com>
2021-03-07 20:28:10 -0600

    ID is moved to the end of the table definition

--- database/tables/messages.sql
+++ database/tables/messages.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS message_store.messages (
   global_position bigserial NOT NULL,
   position bigint NOT NULL,
-  id UUID NOT NULL DEFAULT gen_random_uuid(),
   time TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'utc') NOT NULL,
   stream_name text NOT NULL,
   type text NOT NULL,
   data jsonb,
-  metadata jsonb
+  metadata jsonb,
+  id UUID NOT NULL DEFAULT gen_random_uuid()
 );
 
 ALTER TABLE message_store.messages ADD PRIMARY KEY (global_position) NOT DEFERRABLE INITIALLY IMMEDIATE;
