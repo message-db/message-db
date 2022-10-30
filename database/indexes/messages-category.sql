eff5c329bc3537d858b07f96dd53c745ce17997f
Aaron Jensen <aaronjensen@gmail.com>
2022-10-30 10:50:24 -0400

    Messages category correlation index is a separate index

--- database/indexes/messages-category.sql
+++ database/indexes/messages-category.sql
@@ -2,6 +2,5 @@ DROP INDEX IF EXISTS message_store.messages_category;
 
 CREATE INDEX messages_category ON message_store.messages (
   message_store.category(stream_name),
-  global_position,
-  message_store.category(metadata->>'correlationStreamName')
+  global_position
 );
